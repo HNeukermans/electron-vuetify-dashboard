337567f13c0f57c3417274ed5d0c90ef90072a5b
DELL-PRECISION\Hannes Neukermans <hannes.neukermans@be-mobile.com>
2017-10-25 15:26:32 +0200

    first commit

---
+++ app/src/components/TcpPortList.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-toolbar color="light-blue" dark>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title inset>Tcp ports</v-toolbar-title>
+        </v-toolbar>
+        <v-list>
+          <v-list-tile avatar v-for="item in items" v-bind:key="item.title">            
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">info</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>                    
+        </v-list>
+      </v-card>
+    </v-flex>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          icon: "folder",
+          iconClass: "grey lighten-1 white--text",
+          title: "Photos",
+          subtitle: "Jan 9, 2014"
+        },
+        {
+          icon: "folder",
+          iconClass: "grey lighten-1 white--text",
+          title: "Recipes",
+          subtitle: "Jan 17, 2014"
+        },
+        {
+          icon: "folder",
+          iconClass: "grey lighten-1 white--text",
+          title: "Work",
+          subtitle: "Jan 28, 2014"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
