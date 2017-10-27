8ba15733e13f39b6273fc19ef73d480faf46d8ce
DELL-PRECISION\Hannes Neukermans <hannes.neukermans@be-mobile.com>
2017-10-27 17:02:50 +0200

    refactor: now relies on vue-cli template

---
+++ src/renderer/components/TcpPortList.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-toolbar color="light-blue" dark>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title inset>Tcp ports</v-toolbar-title>
+        </v-toolbar>
+        <v-list>
+          <v-list-tile avatar v-for="item in processes" v-bind:key="item.port">            
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.port }}</v-list-tile-title>
+              {{ item.name }}
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">info</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>                    
+        </v-list>
+      </v-card>
+    </v-flex>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'; // eslint-disable-line no-unused-vars
+
+export default {
+  created() {
+    this.$store.dispatch('Process/listen');
+  },
+  computed: {
+    processes() {
+      const p = this.$store.getters['Process/sort']();
+      console.log(p);
+      return p;
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
